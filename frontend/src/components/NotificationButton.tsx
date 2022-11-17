7394477e8ec28c8fa2f8a64d2dc1e31b7ec61280
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-11-17 17:59:16 -0300

    Reconfigurando frontend

---
+++ frontend/src/components/NotificationButton.tsx
@@ -0,0 +1,9 @@
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src="notification-icon.svg" alt="icon" />
+    </div>
+  )
+}
+
+export default NotificationButton
