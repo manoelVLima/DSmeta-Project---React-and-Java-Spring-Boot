6bf2a0a9df51d3379dd497fc6e55db9cf568842f
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-11-19 12:37:04 -0300

    Api teste

---
+++ backend/src/main/java/com/manoelVlima/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.manoelVlima.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manoelVlima.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
